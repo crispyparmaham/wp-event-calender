10afcbd11e6904eed56143d28c695b11405c5fb4
Lucas Dühr <lucas.duehr@web.de>
2026-03-12 16:58:43 +0100

    pus version 2.0.6

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:  Drag & Drop Event Calendar
  * Description:  Kalenderübersicht für Trainings & Seminare mit Drag & Drop Funktion.
- * Version:      2.0.5
+ * Version:      2.0.6
  * Author:       Lucas Dühr | more than ads
  * Author URI:   https://www.morethanads.de
  * Text Domain:  training-calendar
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || exit;
 
 define( 'TC_PATH',    plugin_dir_path( __FILE__ ) );
 define( 'TC_URL',     plugin_dir_url( __FILE__ ) );
-define( 'TC_VERSION', '2.0.5' );
+define( 'TC_VERSION', '2.0.6' );
 
 require_once TC_PATH . 'includes/admin/settings.php';
 require_once TC_PATH . 'includes/admin/dashboard.php';
